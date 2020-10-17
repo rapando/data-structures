3a99061f0dd076eb1eb6c91d5b5f297ade8ae7f9
Rapando <samsonrapando@gmail.com>
2020-10-18 00:07:23 +0300

    added menu

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "linked-lists/linked-lists.h"
 
-int main() {
-    printf("Data Structures Demo in C!\n");
-    linkedListsEntry();
+void menu();
+
+int main()
+{
+    int menuOption;
+    menu();
+    scanf("%d", &menuOption);
+   
+    switch (menuOption)
+    {
+    case 1:
+        linkedListsEntry();
+        break;
+
+    case 0:
+        exit(0);
+        break;
+
+    default:
+        printf("\n*** Invalid Option ***\n");
+        break;
+    }
+
     return 0;
+}
+
+void menu()
+{
+    printf("Data Structures Demo in C!\n\n");
+    printf("** Choose Menu Option **\n\n");
+    printf("\t1. Linked Lists\n");
+    printf("\t0. Quit");
+    printf("\n _ ");
 }
